3a1ed8017be90390aee002819401de312284257d
Matteo95000 <61450585+Matteo95000@users.noreply.github.com>
2022-10-21 15:47:59 +0200

    Géoloc qui marche (sans route)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:trotinette_electrique_fablab/widgets/gmaps.dart';
+import 'package:google_directions_api/google_directions_api.dart';
 
 void main() {
   runApp(const MyApp());
+  DirectionsService.init('AIzaSyAKZciqbtnw7aSe4lwAGlB-AHw6t4GHW6g');
+
 }
 
 class MyApp extends StatelessWidget {
@@ -25,67 +28,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
-
-
-/*
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Google Maps Demo',
-      home: MapSample(),
-    );
-  }
-}
-
-class MapSample extends StatefulWidget {
-  @override
-  State<MapSample> createState() => MapSampleState();
-}
-
-class MapSampleState extends State<MapSample> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
-        myLocationEnabled: true,
-        myLocationButtonEnabled: true,
-        mapType: MapType.normal,
-        zoomGesturesEnabled: true,
-        zoomControlsEnabled: true,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-    );
-  }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
-}*/
--- lib/widgets/gmaps.dart
+++ lib/widgets/gmaps.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_directions_api/google_directions_api.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,80 +15,230 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Completer<GoogleMapController> _controller = Completer();
-  // on below line we have specified camera position
-  static final CameraPosition _kGoogle = const CameraPosition(
+  final directionsService = DirectionsService();
+  final request = DirectionsRequest(
+    origin: 'Chicago, IL',
+    destination: 'San Francisco, CA',
+    travelMode: TravelMode.driving,
+  );
+
+  final destinationAddressController = TextEditingController();
+  String destinationAddress = "";
+  LatLng? destinationPos;
+
+  Position? _currentPosition;
+  String _currentAddress = "";
+
+  final List<Marker> _markers = <Marker>[];
+  Completer<GoogleMapController> _mapController = Completer();
+
+  CameraPosition _camPos = const CameraPosition(
     target: LatLng(20.42796133580664, 80.885749655962),
     zoom: 14.4746,
   );
 
-  // on below line we have created the list of markers
-  final List<Marker> _markers = <Marker>[
-    Marker(
-        markerId: MarkerId('1'),
-        position: LatLng(20.42796133580664, 75.885749655962),
+  @override
+  void initState() {
+    super.initState();
+    destinationAddressController.addListener(_updateDestination);
+    _initStartPos();
+    /*directionsService.route(request,
+            (DirectionsResult response, DirectionsStatus status) {
+          if (status == DirectionsStatus.ok) {
+            // do something with successful response
+          } else {
+            // do something with error response
+          }
+        });*/
+  }
+
+  _initStartPos() async {
+    await getUserCurrentLocation();
+    log("#####################" + _currentPosition.toString());
+    if(_currentPosition != null) {
+      LatLng currentLatLng = LatLng(_currentPosition!.latitude, _currentPosition!.longitude);
+      Placemark? place = await _getAddressFromPos(LatLng(_currentPosition!.latitude, _currentPosition!.longitude));
+
+      setState(() {
+        _currentAddress = _formatAdressFromPlacemark(place!);
+      });
+
+      _addMarker(currentLatLng, _currentAddress);
+      _setCameraPos(currentLatLng);
+
+      final GoogleMapController controller = await _mapController.future;
+      controller.animateCamera(
+        CameraUpdate.newLatLng(LatLng(_currentPosition!.latitude, _currentPosition!.longitude))
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    destinationAddressController.dispose();
+    super.dispose();
+  }
+
+  _addMarker(LatLng pos, String name){
+    _markers.add(Marker(
+        markerId: MarkerId(_markers.length.toString()),
+        position: pos,
         infoWindow: InfoWindow(
-          title: 'My Position',
+          title: name,
         )
-    ),
-  ];
+    ));
+  }
+
+  _setCameraPos(LatLng pos, {zoom:15.0}){
+    setState(() {
+      _camPos = CameraPosition(
+        target: pos,
+        zoom: zoom,
+      );
+    });
+  }
 
   // created method for getting user current location
-  Future<Position> getUserCurrentLocation() async {
+  Future<Position?> getUserCurrentLocation() async {
     await Geolocator.requestPermission().then((value){
+      log("####### geolocator.then " + value.toString());
     }).onError((error, stackTrace) async {
       await Geolocator.requestPermission();
-      print("ERROR"+error.toString());
+      log("ERROR"+error.toString());
     });
-    return await Geolocator.getCurrentPosition();
+    _currentPosition = await Geolocator.getCurrentPosition();
+    log("############ before return " + _currentPosition.toString());
+    return _currentPosition;
+  }
+
+  void _updateDestination(){
+    setState(() {
+      destinationAddress = destinationAddressController.text;
+    });
+  }
+
+  _formatAdressFromPlacemark(Placemark place){
+    return "${place.name}, ${place.locality}, ${place.postalCode}, ${place.country}";
+  }
+
+  Future<Placemark?> _getAddressFromPos(LatLng pos) async {
+    try {
+      List<Placemark> p = await placemarkFromCoordinates(pos.latitude, pos.longitude);
+      // Taking the most probable result
+      Placemark place = p[0];
+      return place;
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+
+  Future<LatLng> _getPosFromAddress(String address) async {
+    List<Location> destinationPlacemark = await locationFromAddress(address);
+    return LatLng(destinationPlacemark[0].latitude, destinationPlacemark[0].longitude);
+  }
+
+  _setDestination() async {
+    destinationPos = await _getPosFromAddress(destinationAddress);
+    _addMarker(destinationPos!, destinationAddress);
+    _centerRoad();
+  }
+
+  _centerRoad() async {
+    double miny = (_currentPosition!.latitude <= destinationPos!.latitude)
+        ? _currentPosition!.latitude
+        : destinationPos!.latitude;
+    double minx = (_currentPosition!.longitude <= destinationPos!.longitude)
+        ? _currentPosition!.longitude
+        : destinationPos!.longitude;
+    double maxy = (_currentPosition!.latitude <= destinationPos!.latitude)
+        ? destinationPos!.latitude
+        : _currentPosition!.latitude;
+    double maxx = (_currentPosition!.longitude <= destinationPos!.longitude)
+        ? destinationPos!.longitude
+        : _currentPosition!.longitude;
+
+    double southWestLatitude = miny;
+    double southWestLongitude = minx;
+
+    double northEastLatitude = maxy;
+    double northEastLongitude = maxx;
+
+    // Accommodate the two locations within the camera view of the map
+    final GoogleMapController controller = await _mapController.future;
+    controller.animateCamera(
+      CameraUpdate.newLatLngBounds(
+        LatLngBounds(
+          northeast: LatLng(northEastLatitude, northEastLongitude),
+          southwest: LatLng(southWestLatitude, southWestLongitude),
+        ),
+        100.0,
+      ),
+    );
+
   }
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFF0F9D58),
-        // on below line we have given title of app
         title: Text("GFG"),
       ),
       body: Container(
         child: SafeArea(
           // on below line creating google maps
-          child: GoogleMap(
-            // on below line setting camera position
-            initialCameraPosition: _kGoogle,
-            // on below line we are setting markers on the map
-            markers: Set<Marker>.of(_markers),
-            // on below line specifying map type.
-            mapType: MapType.normal,
-            // on below line setting user location enabled.
-            myLocationEnabled: true,
-            // on below line setting compass enabled.
-            compassEnabled: true,
-            // on below line specifying controller on map complete.
-            onMapCreated: (GoogleMapController controller){
-              _controller.complete(controller);
-            },
-          ),
+          child: Column(
+            children: [
+              Container(
+                  height: height * .7,
+                  width: width,
+                  child: GoogleMap(
+                initialCameraPosition: _camPos,
+                markers: Set<Marker>.of(_markers),
+                mapType: MapType.normal,
+                myLocationEnabled: true,
+                compassEnabled: true,
+                onMapCreated: (GoogleMapController controller){
+                  _mapController.complete(controller);
+                },
+              )
+              ),
+              Container(
+                height: height * .06,
+                width: width,
+                child: TextField(
+                  controller: destinationAddressController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Destination',
+                  ),
+                ),
+              ),
+              Container(
+                height: height * .06,
+                  width: width,
+                  child: IconButton(
+                    onPressed: _setDestination,
+                    icon: Icon(Icons.golf_course)
+                )
+              ),
+
+            ],
+          )
+
         ),
       ),
-      // on pressing floating action button the camera will take to user current location
+
+      /*// on pressing floating action button the camera will take to user current location
       floatingActionButton: FloatingActionButton(
         onPressed: () async{
           getUserCurrentLocation().then((value) async {
             log(value.toString());
-            print(value.latitude.toString() +" "+value.longitude.toString());
-
-            // marker added for current users location
-            _markers.add(
-                Marker(
-                  markerId: MarkerId("2"),
-                  position: LatLng(value.latitude, value.longitude),
-                  infoWindow: InfoWindow(
-                    title: 'My Current Location',
-                  ),
-                )
-            );
+            _addMarker(LatLng(value.latitude, value.longitude), "Destination");
 
             // specified current users location
             CameraPosition cameraPosition = new CameraPosition(
@@ -93,14 +246,14 @@ class _HomePageState extends State<HomePage> {
               zoom: 14,
             );
 
-            final GoogleMapController controller = await _controller.future;
+            final GoogleMapController controller = await _mapController.future;
             controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
             setState(() {
             });
           });
         },
         child: Icon(Icons.local_activity),
-      ),
+      ),*/
     );
   }
 }
