826748779c9f376fb015c529c5cd886d01a52a56
antoine <antoine-haller@outlook.fr>
2022-10-25 10:49:43 +0200

    Fonction icone batterie dynamique

--- lib/Infos_patinette.dart
+++ lib/Infos_patinette.dart
@@ -3,10 +3,35 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 int speed = 0;
-int autonomy = 0;
+int autonomy = 50;
 
 void main() {
   runApp(const InfoScreen());
+}
+
+IconData selectBatteryIcon(autonomy){
+  print(autonomy);
+  if(autonomy >= 90){
+    return Icons.battery_full;
+  }else if(autonomy >= 75 && autonomy < 90){
+    return Icons.battery_6_bar;
+  }else if(autonomy >= 60 && autonomy < 75){
+    return Icons.battery_5_bar;
+  }else if(autonomy >= 45 && autonomy < 60){
+    return Icons.battery_4_bar;
+  }else if(autonomy >= 30 && autonomy < 45){
+    return Icons.battery_3_bar;
+  }else if(autonomy >= 15 && autonomy < 30){
+    return Icons.battery_2_bar;
+  }else if(autonomy < 15 ){
+    return Icons.battery_1_bar;
+  }else if(autonomy < 1 ){
+    return Icons.battery_0_bar;
+  }
+  else{
+    return Icons.battery_0_bar;
+  }
+
 }
 
 class InfoScreen extends StatelessWidget {
@@ -14,6 +39,7 @@ class InfoScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return SafeArea(child: Container(
         margin: EdgeInsetsDirectional.only(start: 50, top: 30),
         padding: EdgeInsetsDirectional.all(10),
@@ -31,7 +57,7 @@ class InfoScreen extends StatelessWidget {
         child:
             Row(
               children: [
-                Icon(Icons.battery_full, color: Colors.white),
+                Icon(selectBatteryIcon(autonomy), color: Colors.white),
                 Text(
                   autonomy.toString() + '%',
                   style: TextStyle(
