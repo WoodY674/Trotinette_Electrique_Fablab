46c110323aee9651aad3ecfed6ca0d443c75d80f
antoine <antoine-haller@outlook.fr>
2022-10-25 11:47:24 +0200

    add shadow to infos widget

--- lib/Infos_patinette.dart
+++ lib/Infos_patinette.dart
@@ -52,6 +52,14 @@ class InfoScreen extends StatelessWidget {
               color: Colors.white,
               width: 2.0,
             ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
         ),
 
         child:
