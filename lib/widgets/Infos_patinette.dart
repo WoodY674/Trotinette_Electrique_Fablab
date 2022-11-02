6a7f396444e800090448d5e0ee9f8fe02ec53d7a
antoine <antoine-haller@outlook.fr>
2022-11-02 15:16:04 +0100

    décoration

--- lib/widgets/Infos_patinette.dart
+++ lib/widgets/Infos_patinette.dart
@@ -23,8 +23,7 @@ IconData selectBatteryIcon(autonomy){
     return Icons.battery_1_bar;
   }else if(autonomy < 5 ){
     return Icons.battery_0_bar;
-  }
-  else{
+  }else{
     return Icons.battery_0_bar;
   }
 }
@@ -34,7 +33,7 @@ int gear(speed){
     return 1;
   }else if(speed > 6 && speed <= 15){
     return 2;
-  }else if(speed > 15 && speed <= 25){
+  }else if(speed > 15 && speed <= 30){
     return 3;
   }
   return 0;
@@ -50,13 +49,13 @@ class InfoScreen extends StatefulWidget {
 class _InfoScreen extends State<InfoScreen> {
 
   final TrotinetteUseCase trotUseCase = TrotinetteUseCase();
-  Patinette patinette = Patinette(battery: 100, speed: 0);
+  Patinette patinette = const Patinette(battery: 100, speed: 0);
   int countDownSimulation = 0;
   @override
   void initState() {
     super.initState();
     //futurePatinette = fetchPatinette(widget.userId);
-    Timer _timer = new Timer.periodic(Duration(seconds: 2), (Timer timer) => getTrotinetteData());
+    Timer _timer = Timer.periodic(Duration(seconds: 2), (Timer timer) => getTrotinetteData());
   }
 
   void getTrotinetteData() async {
@@ -74,10 +73,10 @@ class _InfoScreen extends State<InfoScreen> {
   Widget build(BuildContext context) {
 
     return SafeArea(child: Container(
-        margin: EdgeInsetsDirectional.only(start: 50, top: 30),
-        padding: EdgeInsetsDirectional.all(10),
-        height: 60,
-        width: 90 * 3,
+        margin: const EdgeInsetsDirectional.only(start: 70, top: 10),
+        padding: const EdgeInsetsDirectional.all(10),
+        height: 50,
+        width: 80 * 3,
         decoration: BoxDecoration(
             color: Colors.green,
             borderRadius: BorderRadius.circular(25),
